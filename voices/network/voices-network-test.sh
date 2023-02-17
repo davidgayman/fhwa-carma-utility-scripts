d11c1b83ced0a4e80ba227ff7e1249f6b79c878e
David Gayman <David.Gayman@Leidos.com>
2023-02-17 10:04:20 -0500

    Adding voices network related scripts.

---
+++ voices/network/voices-network-test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export << PY
+
+import sys
+print(sys.argv[0])
+print(sys.argv[1])
+print(sys.argv[2])
+print(sys.argv[3])
+
+PY
+
---
+++ voices/network/voices-network.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Provide user-friendly VPN management wrapper
+
+
+
+connect
+check-status
+get-ip
+
+register-service
+delete-service
+
